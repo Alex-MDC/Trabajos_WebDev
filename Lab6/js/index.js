983126cc77948e351ff3685623fded72b9687945
Aesir <a00824388@itesm.mx>
2021-09-28 22:39:51 -0500

    polished jqueries on lab6

--- Lab6/js/index.js
+++ Lab6/js/index.js
@@ -1,45 +1,41 @@
-var addItem = document.getElementById("botonAdd")
 
-
-addItem.addEventListener("click",postItem)
-
-function postItem(event){
+$("#botonAdd").on("click",function(){
+    
     event.preventDefault()
-   // console.log("hi")
-   var inputText = document.getElementById("newText").value
-   var list = document.getElementById("lista")
-    //hacer los elementos para item en cajita
-   var div = document.createElement("div")
-   var li = document.createElement("li")
-   var p = document.createElement("p")
-   var checkBoton = document.createElement("button")
-   var delBoton = document.createElement("button")
-   checkBoton.addEventListener("click", checkText)
-   delBoton.addEventListener("click", deleteText)
+    // console.log("hi")
+    var inputText = document.getElementById("newText").value
+    var list = document.getElementById("lista")
+     //hacer los elementos para item en cajita
+    var div = document.createElement("div")
+    var li = document.createElement("li")
+    var p = document.createElement("p")
+    var checkBoton = document.createElement("button")
+    var delBoton = document.createElement("button")
+    
+    checkBoton.className="checar"
+    checkBoton.id="checarBotonQ"
+    delBoton.className="del"
+    delBoton.id="delBotonQ"
+    p.textContent = inputText
+    p.className="producto"
+    checkBoton.textContent = "check"
+    delBoton.textContent = "delete"
+    li.className="lis"
+ 
+    div.appendChild(li)
+    li.appendChild(p)
+    li.appendChild(checkBoton)
+    li.appendChild(delBoton)
+    list.appendChild(div)
 
-   checkBoton.className="checar"
-   delBoton.className="del"
-   p.textContent = inputText
-   p.className="producto"
-   checkBoton.textContent = "check"
-   delBoton.textContent = "delete"
-   li.className="lis"
-
-   div.appendChild(li)
-   li.appendChild(p)
-   li.appendChild(checkBoton)
-   li.appendChild(delBoton)
-   list.appendChild(div)
-
-}
-
-function checkText(event){
-
-    $(this).parent().toggleClass("chec")
-
-}
-function deleteText(event){
-
+})
+$("#lista").on("click",".del",function(){
     $(this).parent().remove()
+   
+})
+$("#lista").on("click", ".checar",function(){
+    $(this).parent().toggleClass("chec")
+})
 
-}
+
+
